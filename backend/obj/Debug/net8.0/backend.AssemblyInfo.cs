71a60f5bc69cfbec8ec29d702524d40d03829b06
mina18390 <mina.stankovic@elfak.rs>
2026-02-02 14:26:53 +0100

    Phase 2.1: Remove redundant apartment prefetch

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa3b8362fd18b987445a297699c9435a1dd2c6b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3232b4fa7a72edb578ec79e66363c3fe9b510b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
